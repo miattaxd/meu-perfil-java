7e11adb38f0d675a530eee9524a4105dd1525b4c
Nícolas Labate <nicolaslabate61@gmail.com>
2025-02-24 21:22:37 -0300

    Perfil.java

---
+++ Perfil.java
@@ -0,0 +1,12 @@
+public class Perfil {
+    public static void main(String[] args) {
+        String nome = "Nícolas Labate";
+        int idade = 19;
+        String hobby = "Jogar Videogame";
+
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Hobby: " + hobby);
+    }
+}
